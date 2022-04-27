fd5ab8f5eac7c03fc47beba7f122168070dca532
carrout-7 <104464655+carrout-7@users.noreply.github.com>
2022-04-27 09:24:09 -0600

    Add files via upload

---
+++ SUPERHERO/Click Here To Play.py
@@ -0,0 +1,45 @@
+length = input (" LEVEL 1                                     click ENTER to start the game")
+for i in range(int(1000)):
+    print ("START")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print("Batman")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("Superman")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("Black Panther")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("Green Lanteren")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("Iron Man")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("Captan America")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("aquaman")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("AntMan")
+length = input                                   ("type which superhero you think is going to be displayed")
+for i in range(int(100)):
+    print ("Hawkeye")
+length = input                                                 ("click ENTER to go to level 2")
+for i in range(int(1000)):
+    print("THE END")
+length = input ("LEVEL 2                                      which would win Batman or Superman")
+for i in range(int(100)):
+    print ("Superman")
+length = input                                             ("which would win Super or Black Panther")
+for i in range(int(100)):
+    print ("Superman")
+length = input                                         ("which would win Black Panther or green lanteren")
+for i in range(int(100)):
+    print ("Green lanteren")
+length = input                                           ("which would win green lanteren or Iron Man ")
+for i in range(int(100)):
+    print ("Green lanteren")
